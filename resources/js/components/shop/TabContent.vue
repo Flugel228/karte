25539411cb04f8f7c8956419c8f841d3e05fff14
Flugel228 <imflugel@gmail.com>
2023-10-16 23:40:46 +0300

    refactor: Was deleted product popup from TabContent.vue component

--- resources/js/components/shop/TabContent.vue
+++ resources/js/components/shop/TabContent.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {computed, onMounted, ref} from "vue";
+import {computed, onMounted} from "vue";
 import {Product} from "../../types/views/shop";
 import {useStore} from "vuex";
 import {User} from "../../types/store";
@@ -16,11 +16,6 @@ const {t} = useI18n({useScope: 'global'});
 // connection to vuex store
 const store = useStore();
 
-// refs
-const popupProduct = ref<Product>();
-const isActive = ref<boolean>(false);
-const quantity = ref<number>(1);
-
 // computed variables
 const products = computed((): Product[] => store.getters["shop/GET_PRODUCTS"])
 const user = computed((): User => store.getters["GET_USER"]);
@@ -28,13 +23,6 @@ const meta = computed((): Meta => store.getters["shop/GET_META"]);
 
 
 // METHODS
-// popup methods
-const openPopup = (product: Product) => {
-    isActive.value = !isActive.value;
-    popupProduct.value = product
-    quantity.value = 1;
-}
-const closePopup = () => isActive.value = !isActive.value;
 
 // like methods
 const likeHandler = (array: { id: number }[]): boolean => array.some((e: { id: number }): boolean => e.id === user.value.id);
@@ -54,16 +42,6 @@ const likeClickHandler = async (id: number) => {
     }
 }
 
-// product quantity methods
-const incrementQuantity = (): void => {
-    quantity.value++;
-}
-
-const decrementQuantity = (): void => {
-    if (quantity.value > 1) {
-        quantity.value--;
-    }
-}
 
 onMounted(async () => {
     await store.dispatch("shop/FETCH_PRODUCTS", 1);
@@ -139,23 +117,6 @@ onMounted(async () => {
                                                     </span>
                                                 </a>
                                             </li>
-                                            <li>
-                                                <a href="compare.html">
-                                                    <i class="flaticon-left-and-right-arrows"></i>
-                                                    <span>
-                                                        {{ $t('shop.products.tabContent.flaticon.compare') }}
-                                                    </span>
-                                                </a>
-                                            </li>
-                                            <li><a :href="`#popup${id}`"
-                                                   class="popup_link"
-                                                   @click="openPopup(product)"
-                                            > <i
-                                                class="flaticon-visibility"></i>
-                                                <span>
-                                                    {{ $t('shop.products.tabContent.flaticon.quickView') }}
-                                                </span>
-                                            </a></li>
                                         </ul>
                                     </div>
                                 </div>
@@ -392,14 +353,6 @@ onMounted(async () => {
                                         </div>
                                         <div class="icon">
                                             <ul>
-                                                <li>
-                                                    <a
-                                                        @click.prevent="openPopup(product)"
-                                                        class="popup_link"
-                                                    >
-                                                        <i class="flaticon-eye"></i>
-                                                    </a>
-                                                </li>
                                                 <li v-if="user">
                                                     <a
                                                         v-if="likeHandler(product.likedUsers)"
@@ -431,132 +384,6 @@ onMounted(async () => {
             </div>
         </div>
     </div>
-    <template v-if="isActive && popupProduct">
-        <div class="mfp-bg mfp-fade mfp-ready"></div>
-        <div class="mfp-wrap mfp-close-btn-in mfp-auto-cursor mfp-fade mfp-ready" tabindex="-1"
-             style="overflow: hidden auto;">
-            <div class="mfp-container mfp-s-ready mfp-inline-holder">
-                <div class="mfp-content">
-                    <div id="popup5" class="product-gird__quick-view-popup">
-                        <div class="container">
-                            <div class="row justify-content-between align-items-center">
-                                <div class="col-lg-6">
-                                    <div class="quick-view__left-content">
-                                        <div class="tabs ui-tabs ui-corner-all ui-widget ui-widget-content">
-                                            <div class="popup-product-thumb-box">
-                                                <ul role="tablist"
-                                                    class="ui-tabs-nav ui-corner-all ui-helper-reset ui-helper-clearfix ui-widget-header">
-                                                    <li
-                                                        v-for="(image, id) in popupProduct.images"
-                                                        :key="id"
-                                                        class="tab-nav popup-product-thumb ui-tabs-tab ui-corner-top ui-state-default ui-tab ui-tabs-active ui-state-active"
-                                                        role="tab" :tabindex="id" :aria-controls="`tabb${id}`"
-                                                        :aria-labelledby="`ui-id-${id}`" aria-selected="false"
-                                                        aria-expanded="false"
-                                                    >
-                                                        <a :href="`#tabb${id}`" tabindex="-1" class="ui-tabs-anchor"
-                                                           id="ui-id-1">
-                                                            <img :src="image.url" :alt="image.id">
-                                                        </a>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                            <div class="popup-product-main-image-box">
-                                                <div v-for="(image, id) in popupProduct.images"
-                                                     :id="`tabb${id}`"
-                                                     class="tab-item popup-product-image ui-tabs-panel ui-corner-bottom ui-widget-content"
-                                                     :aria-labelledby="`ui-id-${id}`" role="tabpanel"
-                                                     aria-hidden="false"
-                                                     :style="id !== 0 ? {display: 'none'} : ''"
-                                                >
-                                                    <div class="popup-product-single-image">
-                                                        <img :src="image.url" :alt="image.id">
-                                                    </div>
-                                                </div>
-                                                <button class="prev"><i class="flaticon-back"></i>
-                                                </button>
-                                                <button class="next"><i class="flaticon-next"></i>
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6">
-                                    <div class="popup-right-content">
-                                        <h3>{{ popupProduct.title }}</h3>
-                                        <div class="ratting">
-                                            <i class="flaticon-star"></i>
-                                            <i class="flaticon-star"></i>
-                                            <i class="flaticon-star"></i>
-                                            <i class="flaticon-star"></i> <i class="flaticon-star"></i>
-                                            <span>(112)</span></div>
-                                        <p class="text">{{ popupProduct.description }}</p>
-                                        <div class="price">
-                                            <h2> ${{ popupProduct.price }} USD
-                                                <del> ${{ popupProduct.price }} USD</del>
-                                            </h2>
-                                        </div>
-                                        <div class="color-varient">
-                                            <a
-                                                v-for="(color, id) in popupProduct.colors"
-                                                :key="id"
-                                                class="color-name"
-                                                :style="{background: color.code}"
-                                            >
-                                                <span>{{ color.title }}</span>
-                                            </a>
-                                        </div>
-                                        <div class="add-product">
-                                            <h6>Qty:</h6>
-                                            <div class="button-group">
-                                                <div class="qtySelector text-center">
-                                                    <span
-                                                        class="decreaseQty"
-                                                        @click="decrementQuantity"
-                                                    >
-                                                        <i class="flaticon-minus"></i>
-                                                    </span>
-                                                    <input type="number" class="qtyValue" v-model="quantity">
-                                                    <span
-                                                        class="increaseQty"
-                                                        @click="incrementQuantity"
-                                                    >
-                                                        <i class="flaticon-plus"></i>
-                                                    </span>
-                                                </div>
-                                                <button
-                                                    @click.prevent="addToCart(popupProduct, quantity)"
-                                                    class="btn--primary "
-                                                >
-                                                    {{ $t('shop.products.tabContent.buttons.addTo') }}
-                                                </button>
-                                            </div>
-                                        </div>
-                                        <div class="payment-method"><a href="#0"> <img
-                                            src="assets/images/payment_method/method_1.png" alt=""> </a>
-                                            <a href="#0"> <img src="assets/images/payment_method/method_2.png" alt="">
-                                            </a>
-                                            <a href="#0"> <img src="assets/images/payment_method/method_3.png" alt="">
-                                            </a>
-                                            <a href="#0"> <img src="assets/images/payment_method/method_4.png" alt="">
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <button
-                            title="Close (Esc)"
-                            type="button"
-                            class="mfp-close"
-                            @click="closePopup"
-                        >×
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </template>
 </template>
 
 <style scoped lang="sass">
